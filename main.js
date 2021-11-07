e923f294ff4f871d30d20f21b3d03f564aeb8539
Julia Lendzion <juli47g3@stud.kea.dk>
2021-11-07 14:20:38 +0100

    checkboxes not required, js start

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+
+const form = document.querySelector("form");
+form.setAttribute("novalidate", true);
+
+form.addEventListener("submit", (e) => {
+  console.log("przycisk dziala");
+  e.preventDefault();
+  if (form.checkValidity()) {
+    const data = {
+      name: form.elements.name.value,
+      surname: form.elements.surname.value,
+
+      // horns: horns,
+      // species: form.elements.species.value,
+      //   abilities: form.elements.abilities.value.split("\n"),
+    };
+    console.log(data);
+    post(data);
+  } else {
+    form.reportValidity();
+  }
+});
