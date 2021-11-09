848c406a1d96b632fa757c7bdd6437a09d165a11
AnnaCybulska <anna357y@stud.kea.dk>
2021-11-09 14:56:53 +0100

    Branch Home_page_design merged with master

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+module.exports = {
+    base: "./",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sub: resolve(__dirname, "home.html"),
+            },
+        },
+    },
+};
